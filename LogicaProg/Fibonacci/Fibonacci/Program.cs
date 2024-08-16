ce0624799a0769e74343b73b54b18fa42dc6fcde
lyan <127963478+lyanthelyan@users.noreply.github.com>
2024-08-16 09:16:59 -0300

    Add files via upload

---
+++ LogicaProg/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Type the sequence for Fibonacci: ");
+        int sequence = int.Parse(Console.ReadLine());
+
+        int firstNumber = 0; int secondNumber = 1; int nextNumber = 0;
+
+        for (int i = 0; i < sequence; i++)
+        {
+            if (i <= 1)
+            {
+                nextNumber = i; // Enquanto i (0) menor igual ao 1, o proximo numero vai receber i
+            }
+            else
+            {
+                nextNumber = firstNumber + secondNumber; //Calculo para realizar a soma da sequencia
+                firstNumber = secondNumber; // Calculo para os numeros calculaveis ir para frente da sequencia
+                secondNumber = nextNumber;
+            }
+            Console.Write(nextNumber + " ");
+        }
+
+    }
+}
