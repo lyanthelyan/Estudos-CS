d7bee2f11edf2f618f24ee3e714d39fe1448cf47
Elyan <emyo.elyan@gmail.com>
2024-08-16 11:52:48 -0300

    Add Strinbuilder Study

---
+++ Estudos C#/StringBuilder/StringBuilder/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace StringBuilderStudy.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() { }
+        public Comment(string text) { Text = text; }
+    }
+}
---
+++ Estudos C#/StringBuilder/StringBuilder/Entities/Post.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+namespace StringBuilderStudy.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComments(Comment comments)
+        {
+            Comments.Add(comments);
+        }
+        public void RemoveComments(Comment comments)
+        {
+            Comments.Remove(comments);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(); // Para usar Você precisa improtar o using System.Text
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
