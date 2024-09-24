2302f75611d7806c55864196587d8759977f1b84
lyanthelyan <emyo.elyan@gmail.com>
2024-09-24 12:41:06 -0300

    Add Generics

---
+++ Estudos C#/Generics/Generics/Entities/PrintService.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Generics.Entities
+{
+    internal class PrintService<T>//O TIPO É ESCOLHIDO NO PROGRAMA PRINCIPAl, Usando Generics, se eu quisesse que o programa PrintService usasse int, e depois mudasse para string eu teria que mudar o codigo todo
+       //Mas usando generics, mudando o tipo para o tipo T, posso usar tanto string, int, double, sem mudar o coidog todo
+    {
+       private  T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10){ throw new InvalidOperationException("PrintService is Full"); }
+            _values[_count] = value;
+            _count++;
+
+        }
+
+        public T First()
+        {
+            if (_count == 0) { throw new InvalidOperationException("PrintService is Empty"); }
+            return _values[0];
+        }
+        public T Last()
+        {
+            if (_count == 0) { throw new InvalidOperationException("PrintService is Empty"); }
+            return _values[_count-1];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i]+", ");
+            }
+            if(_count>0){ Console.Write(_values[_count-1]); }//Ultima posição do vetor, usando count-1'
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Estudos C#/Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using Generics.Entities;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("How many values: ");
+        int n = int.Parse(Console.ReadLine());
+        
+        //Agora se eu quiser usar um programa que seja para string eu escolho o tipo
+        PrintService<string> s = new PrintService<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string value =Console.ReadLine();
+            s.AddValue(value);
+        }
+
+        s.Print();
+
+        Console.WriteLine("First: "+s.First());
+        Console.WriteLine("Last: " + s.Last());
+
+    }
+}
