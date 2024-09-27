0a7eb68a326250647cc7e4aebea34ebba270dffb
lyanthelyan <emyo.elyan@gmail.com>
2024-09-27 10:16:38 -0300

    Curso de estudantes exercicio Hash-code

---
+++ Estudos C#/CourseHash_Exercise/CourseHash_Exercise/Entitites/Student.cs
@@ -0,0 +1,26 @@
+﻿
+namespace CourseHash_Exercise.Entitites
+{
+    internal class Student
+    {
+        public int Code { get; set; }
+
+        public Student(int code)
+        {
+            Code = code;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Student)) { return false; }
+            Student other = obj as Student;
+            return Code.Equals(other.Code);
+        }
+    }
+}
---
+++ Estudos C#/CourseHash_Exercise/CourseHash_Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using CourseHash_Exercise.Entitites;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        HashSet<Student> students = new HashSet<Student>();
+
+        Console.Write("How many students for course A: ");
+        int c1 = int.Parse(Console.ReadLine());
+        for (int i = 0; i < c1; i++)
+        {
+            int codeA = int.Parse(Console.ReadLine());
+            students.Add(new Student(codeA));
+        }
+
+        Console.Write("How many students for course B: ");
+        int c2 = int.Parse(Console.ReadLine());
+        for (int i = 0; i < c2; i++)
+        {
+            int codeB = int.Parse(Console.ReadLine());
+            students.Add(new Student(codeB));
+        }
+
+        Console.Write("How many students for course C: ");
+        int c3 = int.Parse(Console.ReadLine());
+        for (int i = 0; i < c3; i++)
+        {
+            int codeC = int.Parse(Console.ReadLine());
+            students.Add(new Student(codeC));
+        }
+
+        Console.WriteLine("Total students: " + students.Count());
+
+
+
+
+
+
+    }
+}
